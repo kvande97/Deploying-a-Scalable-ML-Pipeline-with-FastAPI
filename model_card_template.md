b62872ea06a1924b36e515e722c7223b92df7c29
Kyle VandeWiele <kylevandewiele97@gmail.com>
2024-06-10 19:50:07 -0500

    added model_card, built out main.py, ran tests

--- model_card_template.md
+++ model_card_template.md
@@ -1,19 +1,28 @@
 # Model Card
 
-For additional information see the Model Card paper: https://arxiv.org/pdf/1810.03993.pdf
-
 ## Model Details
-
+- This model was created on 06/10/2024 as part of the Udacity Machine Learning DevOps course. 
+- I used a Random Forest Classifier to predict the salary of a person based on the features in the dataset.
 
 ## Intended Use
+- The model can be used to predict the salary (less than or greater than 50k) of a person based on census data.
+- This is an example project of Production ML Model Depolyment, and should not be used in a production environment.
 
 ## Training Data
+- The model was trained on 80% of the census data, which contains information about individuals such as age, education, occupation, and salary.
 
 ## Evaluation Data
+- The model was evaluated on the remaining 20% of the census data.
 
 ## Metrics
-_Please include the metrics used and your model's performance on those metrics._
+- The model was evaluated using precision, recall, and F1 scores. The model achieved: 
+    - Precision: 0.7213
+    - Recall: 0.6353
+    - F1: 0.6756
 
 ## Ethical Considerations
+- The model should not be used to make decisions about individuals, such as hiring or lending, as bias was not tested as a part of this project. 
+
 
 ## Caveats and Recommendations
+- Census data may not be representative of the population as a whole, and the model may not generalize well to other datasets.
--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,55 @@
+
 import pytest
-# TODO: add necessary import
+import pandas as pd
+from ml.data import process_data
+from ml.model import train_model
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
-# TODO: implement the first test. Change the function name and input as needed
-def test_one():
-    """
-    # add description for the first test
-    """
-    # Your code here
-    pass
+data = pd.read_csv('data/census.csv')
+train_data, test_data = train_test_split(data, test_size=0.2, random_state=40)
+
+cat_features = [
+    "workclass",
+    "education",
+    "marital-status",
+    "occupation",
+    "relationship",
+    "race",
+    "sex",
+    "native-country",
+]
+ 
+X_train, y_train, encoder, lb = process_data(
+    train_data,
+    categorical_features=cat_features,
+    label="salary",
+    training=True,
+)
+
+model = train_model(X_train, y_train)
+
+def test_train_model():
+    assert isinstance(model, RandomForestClassifier)
+
+def test_data_shapes():
+    assert data.shape[1] == 15
+    assert train_data.shape[1] == 15
+    assert test_data.shape[1] == 15
+    assert X_train.shape[0] == y_train.shape[0]
+
+def test_split_percentage():
+    perc = train_data.shape[0] / data.shape[0]
+    assert abs(perc - 0.8) < 0.01
+
+def test_clean_data():
+    assert data.isnull().sum().sum() == 0
+
+    
+    
 
 
-# TODO: implement the second test. Change the function name and input as needed
-def test_two():
-    """
-    # add description for the second test
-    """
-    # Your code here
-    pass
 
 
-# TODO: implement the third test. Change the function name and input as needed
-def test_three():
-    """
-    # add description for the third test
-    """
-    # Your code here
-    pass
+
+
